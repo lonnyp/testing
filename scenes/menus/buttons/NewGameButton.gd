24680e2a294349765e5a222818c6bf4db1585f03
lonnyp <lonnyp@users.noreply.github.com>
2020-04-18 15:47:20 -0500

    Updating folder structure

--- scenes/menus/buttons/NewGameButton.gd
+++ scenes/menus/buttons/NewGameButton.gd
@@ -5,6 +5,6 @@ extends Button
 
 
 func _on_NewGameButton_pressed():
-	get_tree().change_scene("res://test/play_ground/world.tscn")
+	get_tree().change_scene("res://scenes/all scenes/world.tscn")
 
 
---
+++ scripts/Actor.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+class_name Actor
+
+
+
+
+
+var gravity = -9.8 * 3
+var max_speed = 8
+var mouse_sensitivity = 0.002  # radians/pixel
+var jump_height = 10
+var velocity = Vector3()
+
